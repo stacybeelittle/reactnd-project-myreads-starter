5d4ae94071ed644c37ae8c3432b167350955cb1f
Stacy <smbridson@gmail.com>
2018-10-08 21:54:47 -0400

    added search function and working categories

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
+
+import { Route } from 'react-router-dom'
+
+
 
 import Main from './components/pages/Main.js';
 
 import SearchPage from './components/pages/SearchPage.js';
 
-import { Route } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-
 class BooksApp extends React.Component {
+
   render() {
     return (
       <div>
